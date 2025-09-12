1d8320d05172c78da432884853cf2d6dd1b69010
Clément Roume <clement.roume@hotmail.com>
2025-09-12 10:58:35 +0200

    Readt Intro 0. Basic application

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,25 @@
+import holbertonLogo from './assets/holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img
+          className="holberton-logo"
+          src={holbertonLogo}
+          alt="holberton logo"
+        />
+        <h1>School Dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
