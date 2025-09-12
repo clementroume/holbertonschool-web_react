a48b469ed98c75fbfb9515dccdd8470e43cf82e8
Clément Roume <clement.roume@hotmail.com>
2025-09-12 18:20:00 +0200

    React Intro 8. Deploy to a GitHub page fix fchavonnet 2

--- react_intro/task_4/dashboard/vite.config.js
+++ react_intro/task_4/dashboard/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base: '/holbertonschool-web_react/',
+  webServer: {
+    command: 'npm run dev -- --host 0.0.0.0',
+    url: 'http://localhost:5173',
+    reuseExistingServer: false,
+    timeout: 120000,
+  },
 });
