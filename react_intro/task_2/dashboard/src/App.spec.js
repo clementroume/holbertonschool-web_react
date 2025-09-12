8777c0a238fb189b9fa54e21b227d7bc093d6ec0
Clément Roume <clement.roume@hotmail.com>
2025-09-12 14:54:08 +0200

    React Intro 4. Test the sign in form

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -20,7 +20,7 @@ test('renders the login paragraph and copyright paragraph', () => {
 
   const currentYear = new Date().getFullYear();
   const copyrightText = screen.getByText(
-    `Copyright ${currentYear} - holberton School`
+    `Copyright ${currentYear} - Holberton School main dashboard`
   );
   expect(copyrightText).toBeInTheDocument();
 });
@@ -31,4 +31,30 @@ test('renders the Holberton logo image', () => {
 
   const imgElement = screen.getByAltText(/holberton logo/i);
   expect(imgElement).toBeInTheDocument();
+});
+
+test('renders 2 input elements (one for email and the other for password', () => {
+  render(<App />);
+
+  const emailInput = screen.getByLabelText(/email/i);
+  expect(emailInput).toBeInTheDocument();
+  const passwordInput = screen.getByLabelText(/password/i);
+  expect(passwordInput).toBeInTheDocument();
+});
+
+test('render 2 label elements with the text "email" and "password"', () => {
+  render(<App />);
+
+  const emailLabel = screen.getByText(/email/i);
+  expect(emailLabel).toBeInTheDocument();
+
+  const passwordLabel = screen.getByText(/password/i);
+  expect(passwordLabel).toBeInTheDocument();
+});
+
+test('Render a button with the text "OK"', () => {
+  render(<App />);
+
+  const button = screen.getByRole('button', { name: /ok/i });
+  expect(button).toBeInTheDocument();
 });
