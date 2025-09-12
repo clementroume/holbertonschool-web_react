1b116acb7a9be260e02c0bf4ddfc18911e73e714
Clément Roume <clement.roume@hotmail.com>
2025-09-12 13:26:26 +0200

    React Intro 3. Modify the App

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -19,6 +19,15 @@ function App() {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" />
+
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" />
+
+          <button type="submit">OK</button>
+        </form>
       </div>
       <div className="App-footer">
         <p>
