81904a147be96c8ac94a223c1ffe0d3eeb8daaa3
Clément Roume <clement.roume@hotmail.com>
2025-09-19 14:49:23 +0200

    React Props 3. Devtool React extension

---
+++ react_props/task_3/dashboard/src/Login/Login.spec.js
@@ -0,0 +1,41 @@
+import Login from './Login';
+import { render, screen } from '@testing-library/react';
+
+test('Render 2 input elements', () => {
+  render(<Login />);
+
+  const emailInput = screen.getByRole('textbox');
+  const passwordInput = screen.getByLabelText(/Password/i);
+
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+});
+
+test('Render 2 label elements with the text "Email:" and "Password:"', () => {
+  render(<Login />);
+
+  const emailLabel = screen.getByText(/email:/i);
+  const passwordLabel = screen.getByText(/password:/i);
+
+  expect(emailLabel).toBeInTheDocument();
+  expect(passwordLabel).toBeInTheDocument();
+});
+
+test('Render a button with the text "OK"', () => {
+  render(<Login />);
+
+  const button = screen.getByRole('button', { name: /ok/i });
+
+  expect(button).toBeInTheDocument();
+});
+
+test('focuses the input when its label is clicked', async () => {
+  const { container } = render(<Login />);
+  const user = userEvent.setup();
+
+  const emailLabel = container.querySelector('label[for="email"]');
+  const emailInput = screen.getByLabelText(/email/i);
+
+  await user.click(emailLabel);
+  expect(emailInput).toHaveFocus();
+});
