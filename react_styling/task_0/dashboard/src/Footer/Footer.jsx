69af30a1f73a417f09ea8b619854d9d8b84fb2fa
Clément Roume <clement.roume@hotmail.com>
2025-10-24 16:13:55 +0200

    fix??

--- react_styling/task_0/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './Footer.css'
-import { getCurrentYear, getFooterCopy } from '../utils/utils'
+import './Footer.css';
+import { getCurrentYear, getFooterCopy } from '../utils/utils';
 
 function Footer() {
-    return (
-        <footer className='App-footer'>
-            <p>Copyright {getCurrentYear()} {getFooterCopy()}</p>
-        </footer>
-    )
+  return (
+    <footer className="App-footer">
+      <p className="semi-emphasized">
+        Copyright {getCurrentYear()} {getFooterCopy()}
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
