94a439e7948449eeb796fe2751b2653662116414
Clément Roume <clement.roume@hotmail.com>
2025-11-25 10:47:28 +0100

    fix x??????????

--- react_redux-part1/task_2/dashboard/src/components/Footer/Footer.spec.js
+++ react_redux-part1/task_2/dashboard/src/components/Footer/Footer.spec.js
@@ -1,51 +1,29 @@
+// External libraries.
 import { render, screen } from '@testing-library/react';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+// Components.
 import Footer from './Footer';
-import { getCurrentYear, getFooterCopy } from '../../utils/utils';
 
-describe('Footer Component', () => {
-    const defaultUser = { isLoggedIn: false, email: '', password: '' };
-    const loggedInUser = { isLoggedIn: true, email: 'test@example.com', password: 'password123' };
-    describe('Basic Rendering', () => {
-        test('Renders without crashing', () => {
-            render(<Footer user={defaultUser} />);
-            const footerParagraph = screen.getByText(`Copyright ${getCurrentYear()} - ${getFooterCopy(true)}`);
-            expect(footerParagraph).toHaveTextContent(/copyright \d{4} - holberton school/i);
-        });
+// Suppress Aphrodite style injection before tests.
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
 
-        test('Does not render contact link when user is not logged in', () => {
-            render(<Footer user={defaultUser} />);
-            const link = screen.queryByRole('link', { name: /contact us/i });
-            expect(link).not.toBeInTheDocument();
-        });
+// Clear and resume style injection after tests.
+afterAll(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
 
-        test('Renders contact link when user is logged in', () => {
-            render(<Footer user={loggedInUser} />);
-            const link = screen.getByRole('link', { name: /contact us/i });
-            expect(link).toBeInTheDocument();
-        });
-    });
+/******************
+* COMPONENT TESTS *
+******************/
 
-    describe('Edge Scenarios', () => {
-        test('does not render contact link when user email is null', () => {
-            const withTruthyIsLoggedIn = { isLoggedIn: true };
-            render(<Footer user={withTruthyIsLoggedIn} />);
-            const link = screen.queryByRole('link', { name: /contact us/i });
-            expect(link).toBeInTheDocument();
-        });
+test('Renders correct copyright text', () => {
+  render(<Footer user={{ isLoggedIn: false }} />);
 
-        test('Does not render contact link when user email is invalid', () => {
-            const withFalsyIsLoggedIn = { isLoggedIn: false };
-            render(<Footer user={withFalsyIsLoggedIn} />);
+  const currentYear = new Date().getFullYear();
+  const footerParagraph = screen.getByText(new RegExp(`copyright ${currentYear}.*holberton school`, 'i'));
 
-            const link = screen.queryByRole('link', { name: /contact us/i });
-            expect(link).not.toBeInTheDocument();
-        });
-    });
-
-    test('Should confirm Footer is a functional component', () => {
-        const FooterPrototype = Object.getOwnPropertyNames(Footer.prototype);
-        expect(FooterPrototype).toEqual(expect.arrayContaining(['constructor']));
-        expect(FooterPrototype).toHaveLength(1);
-        expect(Footer.prototype.__proto__).toEqual({});
-    });
+  expect(footerParagraph).toBeInTheDocument();
 });
